73d0bf81e28e99f45191674f56d0e8d94a13f2f4
Pallavi <PallaviT2001>
2025-09-03 10:27:28 +0530

    newtask

--- PhoneBook_Project_with_files/main.cpp
+++ PhoneBook_Project_with_files/main.cpp
@@ -28,38 +28,3 @@ int main(int argc, char *argv[])
     return app.exec();
 }
 
-
-// #include <QGuiApplication>
-// #include <QQmlApplicationEngine>
-// #include <QQmlContext>
-// #include "ContactModelManager.h"
-
-// int main(int argc, char *argv[])
-// {
-//     QGuiApplication app(argc, argv);
-
-//     //QQmlApplicationEngine engine;
-//     QQmlApplicationEngine engine;
-//     const QUrl url(QStringLiteral("qrc:/PhoneBook_Project_with_files/main.qml"));
-//     engine.load(url);
-
-
-//     ContactModelManager manager;
-//     engine.rootContext()->setContextProperty("modelManager", &manager);
-
-//     //const QUrl url(QStringLiteral("qrc:/Project1/main.qml"));
-//     QObject::connect(
-//         &engine,
-//         &QQmlApplicationEngine::objectCreated,
-//         &app,
-//         [url](QObject *obj, const QUrl &objUrl) {
-//             if (!obj && url == objUrl)
-//                 QCoreApplication::exit(-1);
-//         },
-//         Qt::QueuedConnection);
-//     engine.load(url);
-
-//     return app.exec();
-// }
-
-
