ba14eeff51253c520544d9ffae592f9fd401003c
Pallavi <PallaviT2001>
2025-09-04 10:56:44 +0530

    newtask

--- PhoneBook_Project_with_files/ContactDataModel.h
+++ PhoneBook_Project_with_files/ContactDataModel.h
@@ -1,40 +1,3 @@
-/*#ifndef CONTACTDATAMODEL_H
-#define CONTACTDATAMODEL_H
-#include <QAbstractListModel>
-#include "Contact.h"
-
-class ContactDataModel : public QAbstractListModel
-{
-    Q_OBJECT
-public:
-    enum ContactRoles {
-        NameRole =1,
-        NumberRole,
-        ImageRole,
-        CallTimeRole,
-        IsIncomingRole,
-        IsOutgoingRole,
-        ShortMessageRole
-    };
-
-    explicit ContactDataModel(QObject *parent = nullptr);
-    virtual ~ContactDataModel();
-
-    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
-    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
-    QHash<int, QByteArray> roleNames() const override;
-
-    virtual Contact* createContactData() = 0;
-    virtual void insertContactData(Contact *prototype);
-    virtual Contact* getContactData(int index) const;
-    virtual int count() const;
-
-protected:
-    QList<Contact*> m_contacts;
-};
-
-#endif*/
-
 #ifndef CONTACTDATAMODEL_H
 #define CONTACTDATAMODEL_H
 #include <QAbstractListModel>
@@ -66,7 +29,6 @@ public:
     virtual Contact* getContactData(int index) const;
     virtual int count() const;
 
-    // NEW: basic edit/remove (used by manager)
     virtual void removeAt(int index);
     virtual void updateAt(int index, const QString &name, const QString &number, const QString &image);
 
